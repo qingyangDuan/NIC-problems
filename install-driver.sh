1e0ead66b3078ea583439aaaf6a136860c7831ed
duanqingyang <33786097+duanqingyang@users.noreply.github.com>
2021-05-13 15:57:05 +0800

    Update install-driver.sh

--- install-driver.sh
+++ install-driver.sh
@@ -1,2 +1,6 @@
+# check if nic is shown in lspci and  lshw
+lspci |grep Eth
+lshw -C network 
+
 download driver from https://www.mellanox.com/products/ethernet-drivers/linux/mlnx_en
 tar and install
